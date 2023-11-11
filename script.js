fbd2f247184fdbd87abf0386c79613a61613a10d
hectorconte <contehectore@gmail.com>
2023-11-11 12:00:43 -0300

    finished squeleton

--- script.js
+++ script.js
@@ -48,3 +48,34 @@ let myChart = new Chart(wheel, {
     }
 }
 });
+const valueGenerator = (angleValue) => {
+    for(let i of rotationValues){
+        if(angleValue >= i.minDegree && angleValue <= i.maxDegree){
+            finalValue.innerHTML = `<p>Value: ${i.value}</p>`;
+            spinBtn.disabled = false;
+            break;
+        }
+    }
+}
+let count = 0;
+let resultValue = 101;
+spinBtn.addEventListener("click", () => {
+    spinBtn.disabled = true;
+    finalValue.innerHTML = `<p>Buena suerte!</p>`;
+    let randomDegree = Math.floor(Math.random() * (355 - 0 + 1) + 0);
+    let rotationInterval = window.setInterval(()=> {
+        myChart.options.rotation = myChart.options.rotation + resultValue;
+        myChart.update();
+        if(myChart.options.rotation >= 360){
+            count += 1;
+            resultValue -= 5;
+            myChart.options.rotation = 0;
+        }
+        else if(count > 15 && myChart.options.rotation == randomDegree){
+            valueGenerator (randomDegree);
+            clearInterval(rotationInterval);
+            count = 0;
+            resultValue = 101;
+        }
+    }, 10);
+})
